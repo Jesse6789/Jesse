e0af3cb4aa7c9188b731babd060b7f31f93d2166
Jesse6789 <sweetdream-fly@hotmail.com>
2017-05-11 15:39:06 +0800

    添加app接口controller

---
+++ src/main/java/com/jesse/controller/app/user/UserAct.java
@@ -0,0 +1,16 @@
+package com.jesse.controller.app.user;
+
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * 
+ * @author jesse 2014-11-19
+ * @Email: mmm333zzz520@163.com
+ * @version 3.0v
+ */
+@RestController
+@RequestMapping("/rest/")
+public class UserAct {
+}
